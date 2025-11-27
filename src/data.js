dff66156e2c62895ddabe29bd432f9e9f7bbf915
Izz <iyzzihni@gmail.com>
2025-11-27 10:02:43 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,219 @@
+import HeroImage from "/assets/hero-img.webp";
+
+const Image = {
+  HeroImage,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools2 from "/assets/tools/reactjs.png";
+import Tools3 from "/assets/tools/nextjs.png";
+import Tools4 from "/assets/tools/tailwind.png";
+import Tools5 from "/assets/tools/bootstrap.png";
+import Tools6 from "/assets/tools/js.png";
+import Tools7 from "/assets/tools/nodejs.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools10 from "/assets/tools/canva.png";
+import Tools11 from "/assets/tools/figma.png";
+import Tools13 from "/assets/tools/sql_server.png";
+import Tools14 from "/assets/tools/html.png";
+import Tools15 from "/assets/tools/css.png";
+import Tools16 from "/assets/tools/ts.png";
+import Tools17 from "/assets/tools/php.png";
+import Tools18 from "/assets/tools/laravel.png";
+import Tools19 from "/assets/tools/mysql.png";
+
+
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 2,
+    gambar: Tools2,
+    nama: "React JS",
+    ket: "Framework",
+    dad: "200",
+  },
+  {
+    id: 3,
+    gambar: Tools3,
+    nama: "Next JS",
+    ket: "Framework",
+    dad: "300",
+  },
+  {
+    id: 4,
+    gambar: Tools4,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "400",
+  },
+  {
+    id: 5,
+    gambar: Tools5,
+    nama: "Bootstrap",
+    ket: "Framework",
+    dad: "500",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  {
+    id: 7,
+    gambar: Tools7,
+    nama: "Node JS",
+    ket: "Javascript Runtime",
+    dad: "700",
+  },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "1000",
+  },
+  {
+    id: 11,
+    gambar: Tools11,
+    nama: "Figma",
+    ket: "Design App",
+    dad: "1100",
+  },
+  {
+    id: 13,
+    gambar: Tools13,
+    nama: "SQL Server",
+    ket: "Database",
+    dad: "1300",
+  },
+  {
+    id: 14,
+    gambar: Tools14,
+    nama: "HTML",
+    ket: "Language",
+    dad: "1400",
+  },
+  {
+    id: 15,
+    gambar: Tools15,
+    nama: "CSS",
+    ket: "Language",
+    dad: "1500",
+  },
+  {
+    id: 16,
+    gambar: Tools16,
+    nama: "TypeScript",
+    ket: "Language",
+    dad: "1600",
+  },
+  {
+    id: 17,
+    gambar: Tools17,
+    nama: "PHP",
+    ket: "Language",
+    dad: "1700",
+  },
+  {
+    id: 18,
+    gambar: Tools18,
+    nama: "Laravel",
+    ket: "Framework",
+    dad: "1800",
+  },
+  {
+    id: 19,
+    gambar: Tools19,
+    nama: "MySql",
+    ket: "Database",
+    dad: "1900",
+  },
+];
+
+import Proyek1 from "/assets/proyek/proyek1.jpg";
+
+export const listProyek = [
+  {
+    id: 1,
+    image: Proyek1,
+    title: "Dormitory AstraTech System",
+    subtitle: "Help Administrators monitor student activities within the dormitory...",
+    fullDescription:"Dormitory Astratech is a web-based system designed to help administrators monitor student activities within the dormitory. With a team of five members, we developed the project from business process design to full implementation, ensuring integration with users for proper analysis.",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://github.com/zyhni",
+    dad: "100",
+  },
+  {
+    id: 2,
+    image: Proyek2,
+    title: "Dashboard UPT Informatika",
+    subtitle: "AUPT Informatics is a web-based platform built with Spring Boot Java...",
+    fullDescription:"UPT Informatics is a web-based platform built with Spring Boot Java that streamlines project management, organization, and monitoring within the Informatics Department of Astra Polytechnic, supporting academic staff in achieving efficient and structured project oversight.",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://github.com/zyhni",
+    dad: "200",
+  },
+  {
+    id: 3,
+    image: Proyek3,
+    title: "Internak (Livestock Monitoring Application)",
+    subtitle: "Internak is an Android application designed to help farmers...",
+    fullDescription:"Internak is an Android application designed to help farmers manage their livestock easily. It allows users to monitor animal health, feeding schedules, and farm conditions in real time. The app helps improve efficiency in farm management by providing clear data and automatic monitoring features.",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://github.com/zyhni",
+    dad: "300",
+  },
+  {
+    id: 4,
+    image: Proyek4,
+    title: "Dynamic QRIS Generator",
+    subtitle: "This project is a web-based system that generates dynamic QRIS payment codes...",
+    fullDescription:"This project is a web-based system that generates dynamic QRIS payment codes using the CR16 method. The system creates QR codes in real-time with unique transaction amounts and IDs, ensuring secure and traceable payments.",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://github.com/zyhni",
+    dad: "400",
+  },
+    {
+    id: 5,
+    image: Proyek5,
+    title: "System HR People & Training Development",
+    subtitle: "This system is designed to enable the HR Department to manage employee...",
+    fullDescription:"This system is designed to enable the HR Department to manage employee competencies and help employees track the progress of their completed training programs. The system was developed in collaboration with PT Aisin Indonesia Automotive in Karawang, ensuring flexibility and practical implementation.",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://github.com/zyhni",
+    dad: "500",
+  },
+  {
+    id: 6,
+    image: Proyek6,
+    title: "Roblox Top-up Website",
+    subtitle: "This personally developed Roblox Top-Up Website integrates automatic...",
+    fullDescription:"This personally developed Roblox Top-Up Website integrates automatic QRIS payments with Roblox.API and Midtrans, enabling seamless and secure Robux top-up transactions.",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://github.com/zyhni",
+    dad: "600",
+  },
+];
