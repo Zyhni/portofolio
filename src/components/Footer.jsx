60824845c79a6864855f8cc1f67c6d7343cd3ee8
unknown <iyzzihni@gmail.com>
2026-01-08 09:30:13 +0700

    perbaikan

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,9 +21,9 @@ const Footer = () => {
 
         {/* Ikon Sosmed - di tengah di mobile */}
         <div className="flex gap-3 order-2 md:order-none">
-          <a href="https://github.com/rissss21"><i className="ri-github-fill ri-2x"></i></a>
-          <a href="https://www.instagram.com/edrikprayoga/"><i className="ri-instagram-fill ri-2x"></i></a>
-          <a href="https://www.youtube.com/@FarisEdrikPrayoga"><i className="ri-youtube-fill ri-2x"></i></a>
+          <a href="https://github.com/zyhni"><i className="ri-github-fill ri-2x"></i></a>
+          <a href="https://www.instagram.com/izzzyhni/"><i className="ri-instagram-fill ri-2x"></i></a>
+          <a href="https://www.youtube.com/@zyhnii"><i className="ri-youtube-fill ri-2x"></i></a>
         </div>
 
         {/* Dock - paling bawah di mobile */}
