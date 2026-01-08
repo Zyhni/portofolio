d262b490c4c973283b81f73a2f1acb125a05b126
unknown <iyzzihni@gmail.com>
2026-01-08 08:41:34 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+const isVercel = !!process.env.VERCEL;
+
 export default defineConfig({
-  base: "/portofolio/",
+  base: isVercel ? '/' : '/portofolio/',
   plugins: [react(), tailwindcss()],
 })
